9a06909aa5a2e202a92b6efe1e9b9e06bf4ab0c9
Piotr Budzinski <pbudzinski@hotmail.com>
2026-01-19 18:59:53 +0100

    Definitions

--- Laba4VS/Laba4VS/Laba4VS.cpp
+++ Laba4VS/Laba4VS/Laba4VS.cpp
@@ -10,3 +10,28 @@ int main()
 {
     std::cout << "Inne something";
 }
+
+void initialize()
+{
+    std::cout << "Init game" << std::endl;
+}
+
+void getInput()
+{
+    return;
+}
+
+void update()
+{
+}
+
+void render()
+{
+    std::cout << "render";
+}
+
+void result()
+{
+}
+
+
